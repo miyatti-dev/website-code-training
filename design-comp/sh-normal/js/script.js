28776331e172412f185bb65bb07976ec939b824e
miyatti-dev <miyatti.world@gmail.com>
2022-04-29 10:52:40 +0900

    ブログ、制作実績部分の実装

--- design-comp/sh-normal/js/script.js
+++ design-comp/sh-normal/js/script.js
@@ -1,63 +1,73 @@
 $(function () {
-    var $win = $(window);
-    var fvHeight = $('.fv').outerHeight(); // fvの高さ
-    var imgHeight = $('.fv-image-title-wrapper').outerHeight(); // fv部分のimageの高さ
+  var $win = $(window);
+  var fvHeight = $('.fv').outerHeight(); // fvの高さ
+  var imgHeight = $('.fv-image-title-wrapper').outerHeight(); // fv部分のimageの高さ
 
-    var $headerTitleImage = $('.header-title-image');
-    var $burgerButton = $('.burger-button');
-    var $burgerButtonBar = $('.burger-button__bar');
+  var $headerTitleImage = $('.header-title-image');
+  var $burgerButton = $('.burger-button');
+  var $burgerButtonBar = $('.burger-button__bar');
 
-    var headerTitleImageBlackClass = 'header-title-image-black';
-    var burgerButtonBlackClass = 'burger-button-black';
-    var burgerButtonBarBlackClass = 'burger-button-black__bar';
+  var headerTitleImageBlackClass = 'header-title-image-black';
+  var burgerButtonBlackClass = 'burger-button-black';
+  var burgerButtonBarBlackClass = 'burger-button-black__bar';
 
-    // メニューの色切り替え
-    $win.on('load scroll', function () {
-        var value = $(this).scrollTop();
-        // 左上のTitleImageの色切り替え
-        if (value > imgHeight - 40) {
-            $headerTitleImage.addClass(headerTitleImageBlackClass);
-        } else {
-            $headerTitleImage.removeClass(headerTitleImageBlackClass);
-        }
+  // メニューの色切り替え
+  $win.on('load scroll', function () {
+    var value = $(this).scrollTop();
+    // 左上のTitleImageの色切り替え
+    if (value > imgHeight - 40) {
+      $headerTitleImage.addClass(headerTitleImageBlackClass);
+    } else {
+      $headerTitleImage.removeClass(headerTitleImageBlackClass);
+    }
 
-        // 右上のMenuButtonの色切り替え
-        if (value > fvHeight - 40) {
-            $burgerButton.addClass(burgerButtonBlackClass);
-            $burgerButtonBar.addClass(burgerButtonBarBlackClass);
-        } else {
-            $burgerButton.removeClass(burgerButtonBlackClass);
-            $burgerButtonBar.removeClass(burgerButtonBarBlackClass);
-        }
+    // 右上のMenuButtonの色切り替え
+    if (value > fvHeight - 40) {
+      $burgerButton.addClass(burgerButtonBlackClass);
+      $burgerButtonBar.addClass(burgerButtonBarBlackClass);
+    } else {
+      $burgerButton.removeClass(burgerButtonBlackClass);
+      $burgerButtonBar.removeClass(burgerButtonBarBlackClass);
+    }
 
-        if ($('.burger-button').hasClass('open')) {
-            $burgerButton.removeClass(burgerButtonBlackClass);
-            $burgerButtonBar.removeClass(burgerButtonBarBlackClass);
-        }
-    })
+    if ($('.burger-button').hasClass('open')) {
+      $burgerButton.removeClass(burgerButtonBlackClass);
+      $burgerButtonBar.removeClass(burgerButtonBarBlackClass);
+    }
+  })
 
-    // ハンバーガーメニュー
-    $('.burger-button').on('click', function () {
-        var value = $(window).scrollTop();
-        if (value > fvHeight - 40) {
-            $burgerButton.toggleClass(burgerButtonBlackClass);
-            $burgerButtonBar.toggleClass(burgerButtonBarBlackClass);
-        }
+  // ハンバーガーメニュー
+  $('.burger-button').on('click', function () {
+    var value = $(window).scrollTop();
+    if (value > fvHeight - 40) {
+      $burgerButton.toggleClass(burgerButtonBlackClass);
+      $burgerButtonBar.toggleClass(burgerButtonBarBlackClass);
+    }
 
-        $(this).toggleClass('open');
-        $('.header-nav').fadeToggle(300);
-        $('body').toggleClass('no-scroll');
-    })
+    $(this).toggleClass('open');
+    $('.header-nav').fadeToggle(300);
+    $('body').toggleClass('no-scroll');
+  })
 
-    // メニュー選択時
-    $('.header-nav-list-item__link').on('click', function () {
-        $('.burger-button').removeClass('open');
-        $('.header-nav').fadeOut(300);
-        $('body').removeClass('no-scroll');
-    })
+  // メニュー選択時
+  $('.header-nav-list-item__link').on('click', function () {
+    $('.burger-button').removeClass('open');
+    $('.header-nav').fadeOut(300);
+    $('body').removeClass('no-scroll');
+  })
 
-    $('.faq-list-item__term').click(function () {
-        $(this).toggleClass('open');
-        $(this).next().toggleClass('active');
-    });
+  $('.faq-list-item__term').click(function () {
+    $(this).toggleClass('open');
+    $(this).next().toggleClass('active');
+  });
+
+  const swiper = new Swiper(".works-list", {
+    loop: true, //ループ可能（ループモードを有効に）
+    slidesPerView: 3.8, //スライドを3.6分表示
+    centeredSlides: true, //アクティブなスライドを中央に表示
+    spaceBetween: 56,
+    autoplay: {
+      delay: 5000,
+    }
+  });
 })
