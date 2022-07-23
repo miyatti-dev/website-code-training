3d83aba65765fe598e3b692c3f8a39a3c2599083
miyatti-dev <miyatti.world@gmail.com>
2022-07-23 17:03:30 +0900

    js のコード整理

--- design-comp/sh-normal-ex/js/script.js
+++ design-comp/sh-normal-ex/js/script.js
@@ -5,72 +5,17 @@ AOS.init({
 
 // ready
 $(function () {
-  //////////////////////////////////////////////////
-  // ハンバーガーメニュー
-  //////////////////////////////////////////////////
-
-  function showNavMenu(resize) {
-    const $burgerButton = $('.burger-button');
-    const $headerNav = $('.header-nav');
-    if ($burgerButton.attr('aria-expanded') == 'false') {
-      // burgerButton
-      if (resize != true) {
-        $burgerButton.addClass('open');
-      }
-      $burgerButton.attr('aria-expanded', true);
-
-      // headerNav
-      $headerNav.slideDown(300);
-      $headerNav.attr('area-hidden', 'false');
-    }
-  }
-
-  function hideNavMenu(resize) {
-    const $burgerButton = $('.burger-button');
-    const $headerNav = $('.header-nav');
-    if ($burgerButton.attr('aria-expanded') == 'true') {
-      // burgerButton
-      $burgerButton.removeClass('open');
-      $burgerButton.attr('aria-expanded', false);
-
-      // headerNav
-      if (resize == true) {
-        // リサイズ時はすぐ閉じる
-        $headerNav.slideUp(0);
-      } else {
-        $headerNav.slideUp(300);
-      }
-      $headerNav.attr('area-hidden', 'true');
-    }
-  }
-
-  function resize() {
-    const windowWidth = window.innerWidth;
-    if (windowWidth <= 768) {
-      // メニュー非表示(sp/tablet)
-      hideNavMenu(true);
-    } else {
-      // メニュー表示(pc、tablet)
-      showNavMenu(true);
-    }
-  }
-
   // 読み込み時の初回設定
-  resize();
+  windowResizeEvent();
 
-  $(window).scroll(function () {
-    // windowがスクロールされた時に実行する処理
-    var scrollValue = $(this).scrollTop();
-    if (scrollValue == 0) {
-      $('.header').fadeOut(500);
-    }else{
-      $('.header').fadeIn(500);
-    }
-  });
+  // window scroll event
+  $(window).scroll(windowScrollEvent);
 
-  const $burgerButton = $('.burger-button');
-  const $headerNav = $('.header-nav');
-  $burgerButton.on('click', function () {
+  // window resize event
+  $(window).resize(windowResizeEvent);
+
+  // burger button click
+  $('.burger-button').on('click', function () {
     if ($(this).attr('aria-expanded') == 'true') {
       // メニュー非表示
       hideNavMenu();
@@ -83,16 +28,11 @@ $(function () {
   // メニュー選択時
   $('.header-nav-list-item__link').on('click', function () {
     const windowWidth = window.innerWidth;
+    // メニュー非表示(sp/tablet)
     if (windowWidth <= 768) {
-      // sp/tablet
       hideNavMenu();
     }
   })
-
-  // リサイズ
-  $(window).resize(function () {
-    resize();
-  });
 
   //////////////////////////////////////////////////
   // news tab
@@ -109,22 +49,71 @@ $(function () {
     //data属性値=idが等しいものにclass付与
     $('#' + id).addClass('is-active')
   })
-
-  //////////////////////////////////////////////////
-  // スムーススクロール(ページ内リンクのイベント)
-  //////////////////////////////////////////////////
-  const headerHeight = $('.header').outerHeight(); // headerの高さ
-  $('a[href^="#"]').click(function () {
-    // リンクを取得
-    const href = $(this).attr("href");
-    // ジャンプ先のid名をセット
-    const target = $(href == "#" || href == "" ? 'html' : href);
-    // トップからジャンプ先の要素までの距離を取得
-    const position = target.offset().top - headerHeight;
-    // animateでスムーススクロールを行う
-    $("html, body").animate({
-      scrollTop: position
-    }, 500);
-    return false;
-  });
 })
+
+//////////////////////////////////////////////////
+// ハンバーガーメニュー
+//////////////////////////////////////////////////
+function showNavMenu(resize) {
+  const $burgerButton = $('.burger-button');
+  const $headerNav = $('.header-nav');
+  if ($burgerButton.attr('aria-expanded') == 'false') {
+    // burgerButton
+    if (resize != true) {
+      $burgerButton.addClass('open');
+    }
+    $burgerButton.attr('aria-expanded', true);
+
+    // headerNav
+    $headerNav.slideDown(300);
+    $headerNav.attr('area-hidden', 'false');
+  }
+}
+
+function hideNavMenu(resize) {
+  const $burgerButton = $('.burger-button');
+  const $headerNav = $('.header-nav');
+  if ($burgerButton.attr('aria-expanded') == 'true') {
+    // burgerButton
+    $burgerButton.removeClass('open');
+    $burgerButton.attr('aria-expanded', false);
+
+    // headerNav
+    if (resize == true) {
+      // リサイズ時はすぐ閉じる
+      $headerNav.slideUp(0);
+    } else {
+      $headerNav.slideUp(300);
+    }
+    $headerNav.attr('area-hidden', 'true');
+  }
+}
+
+//////////////////////////////////////////////////
+// window resize event
+//////////////////////////////////////////////////
+function windowResizeEvent() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth <= 768) {
+    // メニュー非表示(sp/tablet)
+    hideNavMenu(true);
+  } else {
+    // メニュー表示(pc、tablet)
+    showNavMenu(true);
+  }
+}
+
+//////////////////////////////////////////////////
+// window scroll event
+//////////////////////////////////////////////////
+function windowScrollEvent() {
+  var scrollValue = $(this).scrollTop();
+  if (scrollValue == 0) {
+    $('.header').fadeOut(500);
+
+    // リサイズを読んでメニュー表示/非表示設定
+    windowResizeEvent();
+  } else {
+    $('.header').fadeIn(500);
+  }
+}
