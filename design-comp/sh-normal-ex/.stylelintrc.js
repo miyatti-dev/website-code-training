89863d2803ac58afe0718ca5cae4c71f255a5e76
miyatti-dev <miyatti.world@gmail.com>
2022-08-12 17:23:59 +0900

    top.scss に stylelint を適用

---
+++ design-comp/sh-normal-ex/.stylelintrc.js
@@ -0,0 +1,13 @@
+﻿module.exports = {
+  extends: [
+    'stylelint-config-standard-scss',
+  ],
+  rules: {
+    "property-no-vendor-prefix": null,
+    "at-rule-no-unknown": [true,
+      {
+        "ignoreAtRules": ["include", "mixin", "each", "use"]
+      }
+    ],
+  }
+}
