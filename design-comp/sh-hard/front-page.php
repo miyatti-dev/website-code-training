706c86946528b39ef60e4c81e67a3f57e6ff9b40
miyatti-dev <miyatti.world@gmail.com>
2022-09-24 12:49:18 +0900

    feature セクション作成

--- design-comp/sh-hard/front-page.php
+++ design-comp/sh-hard/front-page.php
@@ -1,5 +1,6 @@
 <?php get_header() ?>
 
+<!-- about -->
 <section class="section-wrapper section-about">
   <div class="content-wrapper">
     <h2 class="section-title">
@@ -40,11 +41,70 @@
     </div>
   </div>
 </section>
+<!-- //about -->
 
+<!-- feature -->
+<section class="section-wrapper section-feature">
+  <div class="content-wrapper">
+    <h2 class="section-title">
+      TOEFL対策に特化したEngress3つの強み
+    </h2>
+    <ul class="feature-list">
+      <li class="feature-list-item">
+        <div class="feature-list-item__text-wrapper">
+          <p class="feature-list-item__number">
+            特長 １
+          </p>
+          <p class="feature-list-item__feature">
+            TOEFLに最適化された<br>
+            無駄のないカリキュラム
+          </p>
+          <p class="feature-list-item__detail">
+            TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。
+          </p>
+        </div>
+        <div class="feature-list-item__image-wrapper">
+          <img src="<?php echo esc_url(get_theme_file_uri('/image/feature01.jpg')); ?>" alt="" class="feature-list-item__image">
+        </div>
+      </li>
 
+      <li class="feature-list-item">
+        <div class="feature-list-item__text-wrapper">
+          <p class="feature-list-item__number">
+            特長 ２
+          </p>
+          <p class="feature-list-item__feature">
+            日本人指導歴10年以上の<br>
+            経験豊富な講師陣
+          </p>
+          <p class="feature-list-item__detail">
+            Engressの講師陣は、もともと日本人向けにTOEFLを教えていた人が大多数です。また全メンバーがTESOL(英語教授法)を取得しており、知識と経験を兼ね備えている教育のプロフェッショナルです。
+          </p>
+        </div>
+        <div class="feature-list-item__image-wrapper">
+          <img src="<?php echo esc_url(get_theme_file_uri('/image/feature02.jpg')); ?>" alt="" class="feature-list-item__image">
+        </div>
+      </li>
 
-
-
-
+      <li class="feature-list-item">
+        <div class="feature-list-item__text-wrapper">
+          <p class="feature-list-item__number">
+            特長 ３
+          </p>
+          <p class="feature-list-item__feature">
+            平均3ヶ月でTOEFL iBT20点アップ
+          </p>
+          <p class="feature-list-item__detail">
+            Engressは高校生からサラリーマンまで様々な年齢層の方々が通われていますが、完全オーダーメイドのカリキュラムで柔軟に対応しているため、平均3ヶ月でTOEFLスコアを20点アップさせています。
+          </p>
+        </div>
+        <div class="feature-list-item__image-wrapper">
+          <img src="<?php echo esc_url(get_theme_file_uri('/image/feature03.jpg')); ?>" alt="" class="feature-list-item__image">
+        </div>
+      </li>
+    </ul>
+  </div>
+</section>
+<!-- //feature -->
 
 <?php get_footer() ?>
