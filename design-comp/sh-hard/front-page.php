b891415b97bc6d950e5e7bbf81918e6809cc8933
miyatti-dev <miyatti.world@gmail.com>
2022-09-24 12:23:06 +0900

    aboutセクションの追加

--- design-comp/sh-hard/front-page.php
+++ design-comp/sh-hard/front-page.php
@@ -1,5 +1,49 @@
 <?php get_header() ?>
 
+<section class="section-wrapper section-about">
+  <div class="content-wrapper">
+    <h2 class="section-title">
+      TOEFL学習でこんな悩みありませんか？
+    </h2>
+    <ul class="problem-list">
+      <li class="problem-list-item">
+        <p class="problem-list-item__text">
+          勉強の習慣が<br>
+          身についていない
+        </p>
+      </li>
+      <li class="problem-list-item">
+        <p class="problem-list-item__text">
+          勉強しているはず<br>
+          なのに点数が伸びない
+        </p>
+      </li>
+      <li class="problem-list-item">
+        <p class="problem-list-item__text">
+          正しい勉強方法が<br>
+          わからない
+        </p>
+      </li>
+    </ul>
+    <div class="engress-about">
+      <div class="engress-about-inner">
+        <p class="engress-about__title">
+          Engressは<br>
+          <span>TOEFLに特化したスクール</span>です
+        </p>
+        <p class="engress-about__text">
+          完全オーダーメイドで、１人１人の悩みに合わせた最適な指導で<br>
+          TOEFLの苦手分野を克服します。
+        </p>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+
+
 
 
 
