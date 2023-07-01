d29960484a1786837b6232fd8988d0e5ade8975c
miyatti <miyatti.world@gmail.com>
2023-07-01 21:36:22 +0900

    stylelint を追加し、scss の並び替え / 整形を実行

---
+++ react/sh-normal-ex/.stylelintrc.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+  extends: ["stylelint-config-standard-scss", "stylelint-config-recess-order"],
+  rules: {
+    "property-no-vendor-prefix": null,
+    "selector-id-pattern": null,
+    "selector-class-pattern": null,
+    "keyframes-name-pattern": null,
+    "scss/at-mixin-pattern": null,
+    "scss/dollar-variable-pattern": null,
+    "scss/percent-placeholder-pattern": null,
+    "scss/at-extend-no-missing-placeholder": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["include", "mixin", "each", "use"],
+      },
+    ],
+  },
+};
