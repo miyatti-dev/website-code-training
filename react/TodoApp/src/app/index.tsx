b8a63e68fb99b87e5fee397c83e5d843519bf8a4
miyatti <miyatti.world@gmail.com>
2023-08-18 21:45:23 +0900

    以下の対応を行った  - @react-navigation/drawer のパッケージ追加  - トップ画面にドロワー表示    - メニュー項目は「Todo 一覧」と「Todo を作成する」の２つ

--- react/TodoApp/src/app/index.tsx
+++ react/TodoApp/src/app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Todo } from 'modules';
 import TodoListScreen from 'modules/todoList/containers';
 import TodoDetailScreen from 'modules/todoDetail/containers';
@@ -15,6 +16,24 @@ export type RootStackParamList = {
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator();
+
+const HomeDrawer = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="TodoList"
+        component={TodoListScreen}
+        options={{ title: 'Todo 一覧' }}
+      />
+      <Drawer.Screen
+        name="CreateTodo"
+        component={CreateTodoScreen}
+        options={{ title: 'Todo を作成する' }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 const App = () => {
   return (
@@ -23,18 +42,18 @@ const App = () => {
         <Stack.Navigator initialRouteName="TodoList">
           <Stack.Screen
             name="TodoList"
-            component={TodoListScreen}
-            options={{ title: 'Todo一覧' }}
+            component={HomeDrawer}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="TodoDetail"
             component={TodoDetailScreen}
-            options={{ title: 'Todo詳細' }}
+            options={{ title: 'Todo 詳細' }}
           />
           <Stack.Screen
             name="CreateTodo"
             component={CreateTodoScreen}
-            options={{ title: 'Create Todo' }}
+            options={{ title: 'Todo を作成する' }}
           />
         </Stack.Navigator>
       </NavigationContainer>
